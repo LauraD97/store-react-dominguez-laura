0aa51745fe1d86637ccbfb394295897877825602
LauraD97 <alejadombar@hotmail.com>
2021-04-25 18:12:12 -0500

    Updates

---
+++ precache-manifest.25f58c573a9c61f6243c05df44b5d63d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af7853c610bff50f7c89dee296c2e95",
+    "url": "/store-react-dominguez-laura/index.html"
+  },
+  {
+    "revision": "f28e826595f8e1680882",
+    "url": "/store-react-dominguez-laura/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d9a2fa020bbaad787e9a",
+    "url": "/store-react-dominguez-laura/static/css/main.af0edae6.chunk.css"
+  },
+  {
+    "revision": "f28e826595f8e1680882",
+    "url": "/store-react-dominguez-laura/static/js/2.01c7ae87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/store-react-dominguez-laura/static/js/2.01c7ae87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a2fa020bbaad787e9a",
+    "url": "/store-react-dominguez-laura/static/js/main.7752fcec.chunk.js"
+  },
+  {
+    "revision": "abefd308da061c75e6ae",
+    "url": "/store-react-dominguez-laura/static/js/runtime-main.44c55a10.js"
+  },
+  {
+    "revision": "452e765cb6fa1cec8d8bf4c209add1ae",
+    "url": "/store-react-dominguez-laura/static/media/aerolab_logo.452e765c.png"
+  },
+  {
+    "revision": "38f0096262028322b79e737df1ede2dd",
+    "url": "/store-react-dominguez-laura/static/media/blue-bag.38f00962.svg"
+  },
+  {
+    "revision": "5d25161b051c2b56b96e61f1d4453408",
+    "url": "/store-react-dominguez-laura/static/media/card-background.5d25161b.png"
+  },
+  {
+    "revision": "3ed58189b663d0fbeb253328ca8834a0",
+    "url": "/store-react-dominguez-laura/static/media/coin.3ed58189.svg"
+  },
+  {
+    "revision": "56df08e23c00d0efd690e61e13f5f446",
+    "url": "/store-react-dominguez-laura/static/media/cover.56df08e2.png"
+  },
+  {
+    "revision": "3af2b989d3968707bca80d19d206eb40",
+    "url": "/store-react-dominguez-laura/static/media/first.3af2b989.svg"
+  },
+  {
+    "revision": "73cee71425c19b2e3bd0abad48307f31",
+    "url": "/store-react-dominguez-laura/static/media/last.73cee714.svg"
+  },
+  {
+    "revision": "9734d7523bdcceb877b030774ea7f066",
+    "url": "/store-react-dominguez-laura/static/media/message.9734d752.svg"
+  },
+  {
+    "revision": "4965e3e45416cc98b943acd658ed260d",
+    "url": "/store-react-dominguez-laura/static/media/next.4965e3e4.svg"
+  },
+  {
+    "revision": "5864da1d45d9f2909f5443362da4cae6",
+    "url": "/store-react-dominguez-laura/static/media/previous.5864da1d.svg"
+  },
+  {
+    "revision": "55b3108399f867691600f666df32065d",
+    "url": "/store-react-dominguez-laura/static/media/product.55b31083.svg"
+  },
+  {
+    "revision": "c859a6de15bb9c191678967cbb20a0d9",
+    "url": "/store-react-dominguez-laura/static/media/shopping.c859a6de.svg"
+  },
+  {
+    "revision": "b0e62303d2dd57661947a43835b7e1e4",
+    "url": "/store-react-dominguez-laura/static/media/user.b0e62303.svg"
+  },
+  {
+    "revision": "9133482b7fff56a630542fee312f953a",
+    "url": "/store-react-dominguez-laura/static/media/white-bag.9133482b.svg"
+  }
+]);
